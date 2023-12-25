f909e8c05a9d4a4705ed893e139f2871e950ee62
ihsanfirdaus <ihsanfirdaus0221@gmail.com>
2023-12-25 20:00:04 +0700

    update migration table user

--- database/migrations/2023_12_15_125026_create_users_table.php
+++ database/migrations/2023_12_15_125026_create_users_table.php
@@ -18,14 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             
-            $table->string('kode_anggota');
-            $table->string('tempat_lahir');
-            $table->string('tanggal_lahir');
-            $table->string('jenis_kelamin');
-            $table->longText('alamat');
-            $table->string('nomor_telepon');
-            $table->string('foto');
-            $table->string('kelas');
+            $table->string('kode_anggota')->nullable();
             $table->string('tempat_lahir')->nullable();
             $table->string('tanggal_lahir')->nullable();
             $table->string('jenis_kelamin')->nullable();
